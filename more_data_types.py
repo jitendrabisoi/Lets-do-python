1e407a89a284d296a326ffa73537f32d3f62d8aa
Jitendra Bisoi <jitendrabisoi21@gmail.com>
2025-08-15 22:42:45 +0530

    python arr and dict

---
+++ more_data_types.py
@@ -0,0 +1,31 @@
+# arr=[]
+# arr.append(123)
+# arr.append(234)
+# arr.append("jitendra")
+# # print(arr)
+
+# print(f"type of arr[1] is {type(arr[1])}")
+# print(f"type of arr[2] is {type(arr[2])}")
+# print(f"type of arr[0] is {type(arr[0])}")
+
+# marks={}
+# # print(type(marks))
+# marks["math"]=92
+# marks["science"]=85
+# marks["ss"]=88.5
+# marks[123]=100
+# print(marks)
+# print(f"type of math marks is {type(marks["math"])}")
+# print(f"type of ss marks is {type(marks["ss"])}")
+
+
+# list_to_sum={}
+# arr=[1,2,3]
+# list_to_sum[arr]=6
+# print(list_to_sum)
+
+int_to_arr={}
+int_to_arr[1]=[1,2,3]
+print(int_to_arr)
+
+
