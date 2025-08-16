fb292e2bd6a6f0b29e1c44ddaf990c73535cd7ed
Jitendra Bisoi <jitendrabisoi21@gmail.com>
2025-08-16 18:52:35 +0530

    Assignment

---
+++ Assignment_week2.py
@@ -0,0 +1,106 @@
+# Python Basics: Variables, Operators, Input/Output
+# Q.1. Write a Python program to store your name in a variable and print a welcome message.
+# name = "Jitendra"
+# print("Hello", name)
+
+# # Create a variable a = 10 and b = 5. Print their sum, difference, and product.
+
+# a = 10
+# b = 5
+# sum=a+b
+# diff=a-b
+# product=a*b
+# print("sum of a and b is", sum)
+# print("diffrence between a and b is", diff)
+# print("Product of a and b is", product)
+
+# Write a program to take input of your age and display: "You are X years old".
+
+# age=int(input("Enter your age:"))
+# print(f"You are {age} years old")
+
+# Assign three values to variables in one line and print them.
+# name, age, city="Jitendra", "29", "Cuttack"
+# print(name, age, city)
+
+# Write a program to check if two numbers (input by user) are equal.
+# Num1=int(input("enter a number:"))
+# Num2=int(input("enter a number:"))
+# if Num1==Num2:
+#     print("Both the numbers are equal")
+# else:
+#     print("Both the numbers are not equal")
+
+# Use the not operator to reverse a boolean expression.
+    
+
+
+
+# Find the floor division and modulus of 17 and 4.
+# floor=17//4
+# modulus=17%4
+# print(floor)
+# print(modulus)
+
+
+# Take two inputs: a name and a city, and print a formatted string using f-strings.
+# name=input("Enter your name:")
+# city=input("Enter your city:")
+# print(f"My name is {name} and i live in {city}")
+
+
+
+# Check if the character 'e' exists in the string "elephant".
+
+# word="elephant"
+# if "e" in word:
+#     print("e exists in the string elephant")
+# else:
+#     print("e not present")
+
+
+# Use the is operator to compare two identical integers.
+
+# a=5
+# b=5
+# if a is b:
+#     print("a equal to b")
+
+
+
+# Use the += operator to increase a number from 5 to 15 by adding 10.
+
+# a=0
+
+# while a<=10:
+#     print(a)
+#     a+=1
+
+# Write a program to display the result of (6 > 3) and (4 == 4).
+
+# result=(6>3) and (4==4)
+# print(result)
+
+
+# Take a string input and use print() to print each word with a Space in between (use sep=' ').
+
+# name="Jitendra"
+# words=list(name)
+# print(words, sep='_')
+
+# Write a program using input() to take a name and age and print a greeting like: “Hi John, you are 21 years old!
+
+name=input("Enter your name:")
+age=int(input("Enter the age:"))
+print(f"Hi {name}, you are {age} years old")
+
+
+
+
+
+
+
+
+
+
+
